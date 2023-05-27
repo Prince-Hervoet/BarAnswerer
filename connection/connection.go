26fea0e2b5a6570d4fb0aa1492a2e0b5ca73c121
ubuntu <book@100ask.localdomain>
2023-05-27 21:44:06 +0800

    add TCP

--- connection/connection.go
+++ connection/connection.go
@@ -1,9 +1,20 @@
 package connection
 
-import "net"
+import (
+	"net"
+
+	"golang.org/x/sys/unix"
+)
 
 type Connection struct {
 	OpenTimestamp int64
 	ConnectionId  int64
 	Conn          net.Conn
+}
+
+type EpollInfo struct {
+	EpollFd  int
+	EventNum int
+	events   []unix.EpollEvent
+	mp       map[int32]func()
 }
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"ShareMemTCP/connection"
+)
+
+func main() {
+	p := &connection.Pioneer{}
+	p.NetInit(2)
+	for {
+		
+	}		
+}
