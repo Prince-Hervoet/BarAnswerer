f6b36f70b01322b3f8bbf021b79d81ceaee47b3a
Prince-Hervoet <2064128441@qq.com>
2023-05-27 14:31:30 +0000

    fix: #add codes

--- core/linux_open_memory.go
+++ core/linux_open_memory.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"ShareMemTCP/util"
 	"errors"
 	"fmt"
 	"syscall"
@@ -21,10 +22,10 @@ type ShareMemory struct {
 	cap        int
 }
 
-func OpenShareMemory(cap int) *ShareMemory {
-	shmid, _, err := syscall.Syscall(syscall.SYS_SHMGET, 1234, 12, IPC_CREATE|0666)
+func OpenShareMemory(key int, cap int) *ShareMemory {
+	shmid, _, err := syscall.Syscall(syscall.SYS_SHMGET, uintptr(key), uintptr(112), IPC_CREATE|0666)
 	if err != 0 {
-		fmt.Println("open memory error")
+		fmt.Printf("open memory error: %s\n", err.Error())
 		return nil
 	}
 	shmaddr, _, err := syscall.Syscall(syscall.SYS_SHMAT, shmid, 0, 0)
@@ -48,18 +49,33 @@ func (here *ShareMemory) WriteShareMemory(data []byte) error {
 		return errors.New("memory no enough")
 	}
 	for i := 0; i < len(data); i++ {
-		*(*byte)(unsafe.Pointer(here.head)) = data[i]
-		here.head += 1
+		*(*byte)(unsafe.Pointer(here.tail)) = data[i]
+		if here.tail == here.rightLimit {
+			here.tail = here.leftLimit
+		} else {
+			here.tail += 1
+		}
 	}
 	here.size += len(data)
-	fmt.Println(here.size)
 	return nil
 }
 
-func (here *ShareMemory) Reset() {
-
+func (here *ShareMemory) ReadShareMemory(len int) []byte {
+	if here.size == 0 {
+		return make([]byte, 0)
+	}
+	ansLen := util.IntMin(len, here.size)
+	ans := make([]byte, len)
+	for i := 0; i < ansLen; i++ {
+		ans[i] = *(*byte)(unsafe.Pointer(here.head))
+		here.head += 1
+	}
+	here.size -= ansLen
+	return ans
 }
 
-func ReadShareMemory() {
-
+func (here *ShareMemory) Reset() {
+	here.head = here.leftLimit
+	here.tail = here.head
+	here.size = 0
 }
--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"ShareMemTCP/connection"
+	"ShareMemTCP/core"
+	"fmt"
 )
 
 func main() {
-	p := &connection.Pioneer{}
-	p.NetInit(2)
-	for {
-
+	sm := core.OpenShareMemory(87, 4096)
+	test := make([]byte, 10)
+	for i := 0; i < 10; i++ {
+		test[i] = byte(i)
+	}
+	err := sm.WriteShareMemory(test)
+	ans := sm.ReadShareMemory(10)
+	fmt.Print("ans: ")
+	for i := 0; i < len(ans); i++ {
+		fmt.Print(ans[i])
+		fmt.Print(" ")
+	}
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 }
